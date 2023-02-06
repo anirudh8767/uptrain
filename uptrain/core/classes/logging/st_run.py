c46fa6c739de6f9d199df4a4c34e8d9e4ca4ed8d
Vipul Gupta <vipgupta84@gmail.com>
2023-02-05 20:01:12 -0800

    Update st_run.py

--- uptrain/core/classes/logging/st_run.py
+++ uptrain/core/classes/logging/st_run.py
@@ -9,7 +9,7 @@ import json
 import plotly.express as px
 
 
-def return_plotly_fig(y_axis, x_axis='Num predictions', x_log=False, y_log=False):
+def return_plotly_fig(y_axis, x_axis="Num predictions", x_log=False, y_log=False):
     fig = go.Figure()
     fig.update_xaxes(title_text=x_axis)
     fig.update_yaxes(title_text=y_axis)
@@ -38,60 +38,71 @@ st.markdown(st_style, unsafe_allow_html=True)
 sub_dirs = [path[0] for path in os.walk(log_folder)]
 st.sidebar.title("Select dashboards to view")
 for sub_dir in sub_dirs:
-    sub_dir_split = sub_dir.split('/')
-    if sub_dir_split[-2] == 'line_plots':
+    sub_dir_split = sub_dir.split("/")
+    if sub_dir_split[-2] == "line_plots":
         plot_name = sub_dir_split[-1]
-        
+
         if st.sidebar.checkbox(f"Line-plot for {plot_name}"):
             st.markdown(f"### Line chart for {plot_name}")
 
-            # Getting the list of all csv files in streamlit logs 
-            csv_files = [file for path,_,_ in os.walk(sub_dir)
-                            for file in glob(os.path.join(path, "*.csv"))]
+            # Getting the list of all csv files in streamlit logs
+            csv_files = [
+                file
+                for path, _, _ in os.walk(sub_dir)
+                for file in glob(os.path.join(path, "*.csv"))
+            ]
 
             scol1, scol2 = st.columns(2)
             with scol1:
                 x_log = st.checkbox(
-                    "log x", help="Plot x-axis in log-scale",
-                    key=plot_name + 'x'
+                    "log x", help="Plot x-axis in log-scale", key=plot_name + "x"
                 )
             with scol2:
                 y_log = st.checkbox(
-                    "log y", help="Plot y-axis in log-scale",
-                    key=plot_name + 'y'
-                    )
+                    "log y", help="Plot y-axis in log-scale", key=plot_name + "y"
+                )
             fig = return_plotly_fig(plot_name, x_log=x_log, y_log=y_log)
             for i, csv_file in enumerate(csv_files):
                 # Reading the csv file
                 df = pd.read_csv(csv_file)
 
                 # Getting plot_id
-                plot_id = csv_file.split('/')[-1].split('.')[0]
-                fig = fig.add_trace(go.Scatter(
-                                x=df['count'],
-                                y=df[plot_id],
-                                name=str(i) + ", " + plot_id,
-                                ))
+                plot_id = csv_file.split("/")[-1].split(".")[0]
+                fig = fig.add_trace(
+                    go.Scatter(
+                        x=df["count"],
+                        y=df[plot_id],
+                        name=str(i) + ", " + plot_id,
+                    )
+                )
             st.plotly_chart(fig)
             # import pdb; pdb.set_trace()
 
         st.sidebar.markdown("""---""")
 
-
-    if sub_dir_split[-2] == 'histograms':
+    if sub_dir_split[-2] == "histograms":
         plot_name = sub_dir_split[-1]
-        
+
         if plot_name != "umap_and_clusters":
             if st.sidebar.checkbox(f"Histogram for {plot_name}"):
                 st.markdown(f"### Histogram for {plot_name}")
 
-                # Getting the list of all files in streamlit logs 
-                files = [file for path,_,_ in os.walk(sub_dir)
-                                for file in glob(os.path.join(path, "*.json"))]
+                # Getting the list of all files in streamlit logs
+                files = [
+                    file
+                    for path, _, _ in os.walk(sub_dir)
+                    for file in glob(os.path.join(path, "*.json"))
+                ]
 
                 for i, file in enumerate(files):
-                    count = file.split('/')[-1].split('.')[0]
-                    if st.checkbox(f"{plot_name} histogram for count {count}"): 
+                    count = file.split("/")[-1].split(".")[0]
+                    if int(count) >= 0:
+                        if st.checkbox(f"{plot_name} histogram for count {count}"):
+                            f = open(file)
+                            data = json.loads(json.load(f))
+                            fig = go.Figure(data=[go.Histogram(x=data, name=count)])
+                            st.plotly_chart(fig)
+                    else:
                         f = open(file)
                         data = json.loads(json.load(f))
                         fig = go.Figure(data=[go.Histogram(x=data, name=count)])
@@ -100,17 +111,20 @@ for sub_dir in sub_dirs:
             if st.sidebar.checkbox(f"{plot_name}"):
                 st.markdown(f"### {plot_name}")
 
-                # Getting the list of all files in streamlit logs 
-                files = [file for path,_,_ in os.walk(sub_dir)
-                                for file in glob(os.path.join(path, "*.json"))]
+                # Getting the list of all files in streamlit logs
+                files = [
+                    file
+                    for path, _, _ in os.walk(sub_dir)
+                    for file in glob(os.path.join(path, "*.json"))
+                ]
 
                 for i, file in enumerate(files):
-                    count = file.split('/')[-1].split('.')[0]
+                    count = file.split("/")[-1].split(".")[0]
                     if st.checkbox(f"{plot_name} UMAP and Clusters for count {count}"):
                         f = open(file)
                         data = json.loads(json.load(f))
-                        arr = np.array(data['umap'])
-                        clusters = data['clusters']
+                        arr = np.array(data["umap"])
+                        clusters = data["clusters"]
                         x = arr[:, 0]
                         y = arr[:, 1]
                         if arr.shape[1] == 2:
@@ -119,11 +133,10 @@ for sub_dir in sub_dirs:
                             z = arr[:, 2]
                             fig = px.scatter_3d(x=x, y=y, z=z, color=clusters)
                         else:
-                            raise("Umap dimension not 2D or 3D.")
+                            raise ("Umap dimension not 2D or 3D.")
                         st.plotly_chart(fig, use_container_width=True)
                         st.write(
                             f"Number of clusters for count {count}: {len(set(clusters))-1}"
                         )
-                    
-    
+
         st.sidebar.markdown("""---""")
